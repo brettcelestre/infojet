305f2bf02364f31467d34f6a058f6f84e0ce9c2d
Brett Celestre <bcelestre@gmail.com>
2017-05-17 15:13:30 -0700

    Inits Login component

---
+++ src/components/login/Login.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
+
+class Login extends Component {
+
+  render() {
+    return (
+      <div>
+        Login Section
+      </div>
+    );
+  }
+
+}
+
+export default Login;
