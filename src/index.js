db3e45054b81d9f46229c84098b934a2edee920c
Brett Celestre <bcelestre@gmail.com>
2017-05-20 01:32:20 -0700

    Updates route structure

--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
 import App from './components/app/App.js';
+import Header from './components/header/Header.js';
+import Login from './components/login/Login.js';
+import SignUp from './components/signup/SignUp.js';
+
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <Provider>
+    <BrowserRouter>
+      <div>
+        <Route path="/" component={Header} />
+        <Switch>
+          <Route path="/login" component={Login} />
+          <Route path="/signup" component={SignUp} />
+          <Route path="/" component={App} />
+        </Switch>
+      </div>
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
