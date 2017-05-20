fbe35f63a24b09330cf9f1bdc507f9de45dd32b2
Brett Celestre <bcelestre@gmail.com>
2017-05-20 01:33:11 -0700

    Adds Login and SignUp Component

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,15 +1,25 @@
 import React, { Component } from 'react';
+import { Link, Route } from 'react-router-dom';
+
 import logo from '../../logo.svg';
 import './App.css';
+
+import Login from '../login/Login';
+import SignUp from '../signup/SignUp';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to InfoJet</h2>
         </div>
+
+        <Login />
+        <SignUp />
+
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
