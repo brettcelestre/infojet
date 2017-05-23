dab656aa1c33985796afa8558220b487b688e64e
Brett Celestre <bcelestre@gmail.com>
2017-05-22 23:13:12 -0700

    Updates index.js Route structure

--- src/index.js
+++ src/index.js
@@ -1,24 +1,29 @@
+import 'babel-polyfill';
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM, { render } from 'react-dom';
 import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import promise from 'redux-promise';
+
+import reducers from './reducers';
 
 import App from './components/app/App.js';
 import Header from './components/header/Header.js';
 import Login from './components/login/Login.js';
 import SignUp from './components/signup/SignUp.js';
 
-import './index.css';
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 ReactDOM.render(
-  <Provider>
+  <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
       <div>
         <Route path="/" component={Header} />
         <Switch>
+          <Route path="/app" component={App} />
           <Route path="/login" component={Login} />
           <Route path="/signup" component={SignUp} />
-          <Route path="/" component={App} />
         </Switch>
       </div>
     </BrowserRouter>
